1931f95f24b71582e375a38210c9bcbdcfaeb74d
hubertDgomes <gomeshubert8@gmail.com>
2026-08-16 03:06:50 +0600

    Add CORS middleware to handle cross-origin requests
    - Integrated `cors` package to manage CORS settings.
    - Configured allowed origins and methods for requests.
    - Updated package.json and package-lock.json to include `cors` dependency.
    - Added necessary CORS headers in the server response.

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ import userRoutes from './routes/userRoutes.js';
 import noteRoutes from './routes/noteRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 import postRoutes from './routes/postRoutes.js';
+import cors from 'cors'
+
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:5173'
+];
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.get('/', (req, res) => {
     res.json({ message: 'The server is working' });
