c9cd76597cae8d80cf0067c17e82822d43630375
hubertDgomes <gomeshubert8@gmail.com>
2026-08-16 03:24:48 +0600

    Update allowedOrigins to remove dynamic frontend URL and ensure consistent CORS configuration

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  process.env.FRONTEND_URL || ''
-].filter(Boolean);
+  "https://note-taking-client.vercel.app"
+]
 
 app.use(express.json());
 app.use(cookieParser());
