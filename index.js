6f0e1a5a537ad22f1545c72994603f84570019f3
hubertDgomes <gomeshubert8@gmail.com>
2026-08-16 03:27:27 +0600

    Update allowedOrigins to ensure consistent CORS configuration

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  "https://note-taking-client.vercel.app"
-]
+  'https://note-taking-client.vercel.app',
+  process.env.FRONTEND_URL || ''
+].filter(Boolean);
 
 app.use(express.json());
 app.use(cookieParser());
